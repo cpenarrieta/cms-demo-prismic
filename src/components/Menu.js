dac28a581b956dc606dd3b33da0712213a1ea9af
Cristian Penarrieta <cristian.penarrieta@gusto.com>
2020-05-01 13:09:47 -0700

    move Menu to CMS

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,25 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useLocale } from "./LocaleContext";
+import gql from "graphql-tag";
+import useCmsQuery from "../hooks/useCmsQuery";
+
+const MENUS_CMS_QUERY = gql`
+  query allMenus($locale: String!) {
+    allMenus(lang: $locale) {
+      edges {
+        node {
+          menus {
+            title
+            route
+          }
+        }
+      }
+    }
+  }
+`;
 
 function Menu() {
   const { locale, setLocale } = useLocale();
+  const { loading, data } = useCmsQuery(MENUS_CMS_QUERY);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  const cmsData = data?.allMenus?.edges?.[0]?.node;
 
   return (
     <nav>
       <ul className="menu">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/add-address">Add Address</Link>
-        </li>
-        <li>
-          <Link to="/add-employee">Add Employees</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
+        {cmsData.menus.map((menu) => (
+          <li>
+            <Link to={menu.route}>{menu.title}</Link>
+          </li>
+        ))}
         <li>
           <select
             value={locale}
